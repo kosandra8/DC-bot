6ca7f7d0257afee7a9853cb09ac8572484da6566
Jessica <gal0004@officersc.vic.edu.au>
2021-08-25 20:23:21 +1000

    Adding bot latency feature

--- commands/ping.js
+++ commands/ping.js
@@ -4,18 +4,21 @@ module.exports = {
     description: "this is a ping command!", 
     async run(client, message, args, Discord) {
         let embed = new Discord.MessageEmbed()
-        .setTitle("Calculating ping...")
-        .setColor('#3E0E52')
+        .setTitle("Pinging...")
+        .setColor('#3E0E52') 
+        message.channel.send('pong! :ping_pong:');
         message.channel.send(embed).then((resultMessage)  => {
             const ping = resultMessage.createdTimestamp - message.createdTimestamp;  
 
             let resultEmbed = new Discord.MessageEmbed()
             .setColor('#3E0E52')
-            .addField(`:robot:  ${ping}ms`, ' || ||')
-            .addField(`:stopwatch: ${client.ws.ping}ms`,' || ||')
+            .addField(`:robot:  ${ping}ms`, ' ** **')
+            .addField(`:stopwatch: ${client.ws.ping}ms`,' ** **')
             
-
+            
+            
             resultMessage.edit(resultEmbed);
+            
              })    
         }
     }
