75afc31e59630afaecce39333dc543825b357726
Jessica <gal0004@officersc.vic.edu.au>
2021-08-30 22:51:18 +1000

    Update commands list

--- commands/commandlist.js
+++ commands/commandlist.js
@@ -3,13 +3,13 @@ module.exports = {
     description: "Help Embed",
     execute(message, args, Discord) {
         const newEmbed = new Discord.MessageEmbed()
-        .setColor('#EE6A50')
+        .setColor('#16161d')
         .setTitle('Oracle Command List')
         .setDescription('Here are some really cool authentic commands, use `O{command}`')           
         .addFields(
             {name: ':bookmark_tabs: Text Commands', value :'`<3` `ping` `mwah` `lol` `69` '},
-            {name: ':island: Images Commands', value :'`avatar` `gif + (anything)`'},
-            {name: ':joy: Fun Commands', value :'`bully` `blush` `clap` `compliment` `cry` `dance` `hug` `hungry` `kick` `kill` `kiss` `laugh` `quote` `rickroll` `sing` `slap` `smirk` `wink`'},
+            {name: ':island: Images Commands', value :'`avatar` `gif + (anything)` `rickroll`'},
+            {name: ':joy: Fun Commands', value :'`blush` `bully` `clap` `compliment` `cry` `dance` `hug` `hungry` `kick` `kill` `kiss` `laugh` `quote` `sing` `slap` `smirk` `wink`'},
             {name: ':notepad_spiral: Math Commands', value :'`calculate "+" "-" "*" "/" "pi"`'},
             
             ) 
