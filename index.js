56f383a6ca973f93c33c510a067c52c87d5fefbb
Jessica <gal0004@officersc.vic.edu.au>
2021-08-30 22:51:36 +1000

    Update command handlers

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 require('dotenv').config();
 
 const client = new Discord.Client();
-const prefix = '<3';
+const prefix = 'O';
 const fs = require('fs');
 
 client.commands = new Discord.Collection();
@@ -15,8 +15,8 @@ for(const file of commandFiles){
 } 
 
 client.once('ready', () => {
-    console.log('jessie <3 is online!')
-     client.user.setActivity('<3help', {type: "WATCHING"}).catch(console.error)
+    console.log('Oracle is online!')
+     client.user.setActivity('Ohelp', {type: "PLAYING"}).catch(console.error)
 
 });
 
@@ -27,22 +27,43 @@ client.on('message', message => {
 
     const args = message.content.slice(prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
-    if (command === 'calculate'){
-    client.commands.get('calculate').run(client, message, args);
-    }else if (command === 'ping') {
+    if(command === 'help'){
+    client.commands.get('commandlist').execute(message, args, Discord);
+    }
+
+//Text Commands     
+    else if (message.content === '<3') {
+    message.reply('Hey I think you dropped this :crown:')
+    } else if (command === 'ping') {    
     client.commands.get('ping').run(client, message, args, Discord);
-    } else if(command === 'help'){
-        client.commands.get('commandlist').execute(message, args, Discord);
-    } else if (message.content === '<3') {
-            message.reply('Hey I think you dropped this :crown:')
-    }else if (command === 'avatar') {
-        client.commands.get('avatar').execute(message, args, Discord);
+    } else if (command === 'mwah'){
+    client.commands.get('kiss').execute(message, args);
+    } else if (command === 'lol'){
+    client.commands.get('lol').execute(message, args);    
+    } else if (command === '69'){
+    client.commands.get('noice').execute(message, args);
+
+//Image Commands
+    } else if (command === 'gif'){
+    client.commands.get('gifs').execute(message, args);
+    } else if (command === 'avatar') {
+    client.commands.get('avatar').execute(message, args, Discord);
+    }  else if (command === 'rickroll'){
+    client.commands.get('rickroll').execute(message, args);
+
+//Fun Commands
+    } else if (command === 'blush'){
+        client.commands.get('blush').execute(message, args, Discord);
     } else if (command === 'bully'){
         client.commands.get('bully').execute(message, args, Discord);
+    } else if (command === 'clap'){
+        client.commands.get('clap').execute(message, args, Discord);
     } else if (command === 'compliment'){
         client.commands.get('compliment').execute(message, args);
-    } else if (command === 'gif'){
-        client.commands.get('gifs').execute(message, args);
+    } else if (command === 'cry'){
+        client.commands.get('cry').execute(message, args, Discord);
+    } else if (command === 'dance'){
+        client.commands.get('dance').execute(message, args, Discord);
     } else if (command === 'hug'){
         client.commands.get('hug').execute(message, args, Discord);
     } else if (command === 'hungry'){
@@ -53,23 +74,28 @@ client.on('message', message => {
         client.commands.get('kill').execute(message, args, Discord);
     } else if (command === 'kiss'){
         client.commands.get('kissgif').execute(message, args, Discord);
-    } else if (command === 'lol'){
-        client.commands.get('lol').execute(message, args);
-    } else if (command === 'mwah'){
-        client.commands.get('kiss').execute(message, args);
-    } else if (command === 'ih8u'){
-        client.commands.get('love').execute(message, args);
-    } else if (command === '69'){
-        client.commands.get('noice').execute(message, args);
+    } else if (command === 'laugh'){
+        client.commands.get('laugh').execute(message, args, Discord);
+    } else if (command === 'purge'){
+        client.commands.get('purge').execute(message, args);
     } else if (command === 'quote'){
-        client.commands.get('quote').execute(message, args);
-    } else if (command === 'rickroll'){
-        client.commands.get('rickroll').execute(message, args);
+        client.commands.get('quote').execute(message, args);           
+    } else if (command === 'sing'){
+        client.commands.get('sing').execute(message, args, Discord);
     } else if (command === 'slap'){
         client.commands.get('slap').execute(message, args, Discord);
-    } else if (command === 'smh'){
-        client.commands.get('smh').execute(message, args);
+    } else if (command === 'smirk'){
+        client.commands.get('smirk').execute(message, args, Discord);
+    }  else if (command === 'wink'){
+        client.commands.get('wink').execute(message, args, Discord);
+    
+
+//Math Commands
+    }  else if (command === 'calculate'){
+    client.commands.get('calculate').run(client, message, args); 
     }
+
+    console.log(command);
     
 });
 
